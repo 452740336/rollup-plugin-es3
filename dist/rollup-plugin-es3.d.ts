92d1ee06745429c60745106d0509572845c85d12
452740336 <11702061+452740336@users.noreply.github.com>
2026-06-14 13:32:10 +0800

    fix: add TypeScript declaration entrypoint

---
+++ dist/rollup-plugin-es3.d.ts
@@ -0,0 +1,21 @@
+declare namespace es3 {
+  interface Options {
+    remove?: Array<"defineProperty" | "freeze" | string>;
+    sourceMap?: boolean;
+    sourcemap?: boolean;
+  }
+
+  interface BundleTransformResult {
+    code: string;
+    map: unknown;
+  }
+
+  interface Plugin {
+    name: "es3";
+    transformBundle(code: string): BundleTransformResult;
+  }
+}
+
+declare function es3(options?: es3.Options): es3.Plugin;
+
+export = es3;
